a8026141d2e01a32ed063c1fa3feca1ae89d1980
michael mwebaze <michael.mwebaze@gmail.com>
2018-10-24 03:44:47 -0400

    added entities

---
+++ modules/constant_contact_campaign/src/Entity/MessageFooter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\constant_contact_campaign\Entity;
+
+
+class MessageFooter {
+  protected $organization_name;
+  protected $address_line_1;
+  protected $address_line_2;
+  protected $address_line_3;
+  protected $city;
+  protected $state;
+  protected $international_state;
+  protected $postal_code;
+  protected $country;
+  protected $include_forward_email;
+  protected $forward_email_link_text;
+  protected $include_subscribe_link;
+  protected $subscribe_link_text;
+}
